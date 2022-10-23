47d7976a4f9a94383e54ecacdf31cf9f5459a9b5
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2022-10-23 19:56:36 +0200

    contact class

--- Program.cs
+++ Program.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 
+//słownik użytkowników
+//metoda dodawania kontaktów
+//metoda wyświetl po numerze
+//metoda wyświetl wszystkie + sortowanie
+//metoda pokaż kontakt po nazwie
+
 namespace PhoneBook
 {
     class Program
