9bf0f768163f73f8c0ae5a657a2f730aa8afcf3f
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2022-10-23 20:41:02 +0200

    show all method

--- Contact.cs
+++ Contact.cs
@@ -9,12 +9,28 @@ namespace PhoneBook
     public class Contact
     {
         public string Name { get; set; }
+        public string LastName { get; set; }
         public string Number { get; set; }
 
-        public Contact(string name, string number)
+        public Contact(string name, string lastName, string number)
         {
-            Name = name;
-            Number = number;
+            if (name.Length > 1 && lastName.Length > 1)
+            { 
+                Name = name;
+                LastName = lastName;
+            }
+            else
+                Console.WriteLine("Name or last name is too short");
+
+            if (number.Length == 9)
+                Number = number;
+            else if(number.StartsWith("0"))
+                Console.WriteLine("Number must be 9 digits and cant start with 0");
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {LastName} {Number}";
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 
-//słownik użytkowników
-//metoda dodawania kontaktów
 //metoda wyświetl po numerze
 //metoda wyświetl wszystkie + sortowanie
 //metoda pokaż kontakt po nazwie
@@ -12,9 +10,71 @@ namespace PhoneBook
 {
     class Program
     {
+        //static Dictionary<string, Contact> GetContacts()
+        //{
+        //    return new Dictionary<string, Contact>
+        //    {
+        //        {"Marta",new Contact("Marta","516421882")},
+        //        {"Darek",new Contact("Darek","779845671")},
+        //        {"Ania",new Contact("Ania","456789121")},
+        //        {"Marek",new Contact("Marek","678098667")},
+        //        {"Danuta",new Contact("Danuta","432117980")},
+        //        {"Piotr",new Contact("Piotr","523112786")},
+        //        {"Czesio",new Contact("Czesio","898760112")},
+        //        {"Michał",new Contact("Michał","672456989")},
+        //        {"Gabi",new Contact("Gabi","765215678")},
+        //        {"Patryk",new Contact("Patryk","442567812")},
+        //    };
+        //}
+
+        static void ShowAllContacts(Dictionary<string, Contact> contacts)
+        {
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine(contact);
+            }
+        }
+        static Dictionary<string, Contact> AddContact(Dictionary<string, Contact> contacts)
+        {
+            Console.WriteLine("Write new contact name:");
+            string nameInput = Console.ReadLine();
+            Console.WriteLine("Write new contact last name:");
+            string lastNameInput = Console.ReadLine();
+            Console.WriteLine("Write new contact number:");
+            string numberInput = Console.ReadLine();
+
+            contacts.Add(nameInput,new Contact(nameInput,lastNameInput,numberInput));
+
+            return contacts;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, Contact> contacts = new Dictionary<string, Contact>
+            {
+                {"Marta",new Contact("Marta","Rozenek","516421882")},
+                {"Darek",new Contact("Darek","Kowal","779845671")},
+                {"Ania",new Contact("Ania","Adamczyk","456789121")},
+                {"Marek",new Contact("Marek","Kowalski","678098667")},
+                {"Danuta",new Contact("Danuta","Słowik","432117980")},
+                {"Piotr",new Contact("Piotr","Jakóbczyk","523112786")},
+                {"Czesio",new Contact("Czesio","Wawrzyniak","898760112")},
+                {"Michał",new Contact("Michał","Owca","672456989")},
+                {"Gabi",new Contact("Gabriela","Prus","765215678")},
+                {"Patryk",new Contact("Patryk","Kołodziejczyk","442567812")},
+            };
+
+            Console.WriteLine("Hello in contact book, what you want to do?" +
+                " \n 1. Show all contacts" +
+                " \n 2. Add new contact");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    ShowAllContacts(contacts);
+                    break;
+
+                default: Console.WriteLine("Invalid instruction");
+                    break;
+            }
         }
     }
 }
